dad9ab854adb78155f6a8197194909c331a20c5f
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2023-07-16 20:57:00 +0300

    changed Facts sectioon

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -63,9 +63,12 @@ echo '" class="h-100">';
 
     <!-- Template Main JS File -->
     <script src="js/main.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 
     <link rel="stylesheet" href="js/css/style.css"/>
     <link rel="stylesheet" href="js/css/glightbox.css"/>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
 
     <?php $this->head() ?>
 </head>
--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -245,6 +245,7 @@ if (in_array($language, $acceptLang)) {
         </div>
     </section><!-- End My Resume Section -->
 
+
     <!-- ======= Facts Section ======= -->
     <section id="facts" class="facts">
         <div class="container">
@@ -254,93 +255,69 @@ if (in_array($language, $acceptLang)) {
             </div>
 
 
-            <div class="row">
-                <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
-                <div class="row m-0">
-                    <div class="col-xs-12 col-md-6 mb-4">
-
-                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                        class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                        aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                        aria-label="Slide 3"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-                                        aria-label="Slide 4"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
-                                        aria-label="Slide 5"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
-                                        aria-label="Slide 6"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
-                                        aria-label="Slide 7"></button>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="img/plants1.jpg" class="d-block w-100" alt="plants">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/guitar.jpg" class="d-block w-100" alt="guitar">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/languages.jpg" class="d-block w-100" alt="languages">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/tea.jpg" class="d-block w-100" alt="tea">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/cat.jpg" class="d-block w-100" alt="cat">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/winner.jpg" class="d-block w-100" alt="competition">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="img/soup.jpg" class="d-block w-100" alt="soup">
-                                </div>
-                            </div>
-                            <button class="carousel-control-prev" type="button"
-                                    data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button"
-                                    data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-
+            <div class="swiper mySwiper2">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="img/plants1.jpg" />
+                        <div class="label">
+                            <?= Yii::t('translations', 'I am addicted to plants') ?>
                         </div>
 
                     </div>
-                    <div class="col-xs-12 col-md-6">
-                        <ul class="mx-auto customPadding">
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I am addicted to plants') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I play guitar and piano') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I studied 6 languages (although some of them remained at the elementary level)') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I drink a looooot of tea') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I dream of having a cat') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'In 2021 I won an award from \'She is Science\' for the best essay about female scientist') ?>
-                            </li>
-                            <li>
-                                <i class="fa fa-chevron-right"></i> <?= Yii::t('translations', 'I enjoy cooking, especially soups') ?>
-                            </li>
-
-                        </ul>
+                    <div class="swiper-slide">
+                        <img src="img/cat.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'I dream of having a cat') ?></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/piano.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'I play guitar and piano') ?></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/languages.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'I studied 6 languages (although some of them remained at the elementary level)') ?></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/soup.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'I enjoy cooking, especially soups') ?></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/tea.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'I drink a looooot of tea') ?></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/winner.jpg" />
+                        <div class="label"><?= Yii::t('translations', 'In 2021 I won an award from \'She is Science\' for the best essay about female scientist') ?></div>
                     </div>
                 </div>
-            </div><!-- End .content-->
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </div>
+            <div thumbsSlider="" class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="img/plants1.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/cat.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/piano.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/languages.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/soup.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/tea.jpg" />
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="img/winner.jpg" />
+                    </div>
+                </div>
+            </div>
 
         </div>
     </section><!-- End Random facts Section -->
@@ -553,6 +530,7 @@ if (in_array($language, $acceptLang)) {
                 });
             </script>
     </section>
+
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
